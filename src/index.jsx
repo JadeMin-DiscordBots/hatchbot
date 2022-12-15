09588962fa1061fe1131c1c1390e27b5598618df
JadeMin <bokmook01@gmail.com>
2022-12-16 01:09:07 +0900

    update

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import { createHandler, authorizeResponse, r } from 'slshx';
+import { createHandler, authorizeResponse } from 'slshx';
 import * as commands from "./interactions/commands";
 
 
@@ -8,4 +8,16 @@ const handler = createHandler({
 	applicationSecret: BOT_TOKEN,
 	commands,
 });
-export default {fetch: handler};
+//export default {fetch: handler};
+export default {
+	async fetch(request, env, ctx) {
+		const { pathname } = new URL(request.url);
+		if(pathname === "/interactions") {
+			return handler(request, env, ctx);
+		} else if(pathname === "/invite") {
+			return authorizeResponse(request, env, ctx);
+		} else {
+			return new Response("No Bitches lol", {status: 404});
+		}
+	}
+}
