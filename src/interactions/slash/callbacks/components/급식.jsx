5c3210a32f6d560d3906d2ca4b869c2cdc7a4173
JadeMin <bokmook01@gmail.com>
2022-12-24 07:54:42 +0900

    테스트

--- src/interactions/slash/callbacks/components/급식.jsx
+++ src/interactions/slash/callbacks/components/급식.jsx
@@ -4,28 +4,32 @@ import {
 } from 'slshx';
 
 
+
+export const MenuEmbed = (props) => (
+	<Embed
+		title={props.title}
+		footer={props.footer}
+	>
+		{props.data.DDISH_NM.replace(/(\.|\s{2})|\([0-9가-힣/.]+\)/g, '').replace(/\<br\/?\>/g, '\n')}
+	</Embed>
+);
+export const NutritionEmbed = (props) => (
+	<Embed
+		title={props.title}
+		footer={`총 ${props.data.CAL_INFO.toLowerCase()}`}
+	>
+		{props.data.NTR_INFO.split(/\<br\/?\>/).map(ntr=> {
+			const [key, value] = ntr.split(" : ");
+			return (
+				<Field name={key} inline>
+					{value}
+				</Field>
+			);
+		})}
+	</Embed>
+);
+
 export default {
-	MenuEmbed: (props) => (
-		<Embed
-			title={props.title}
-			footer={props.footer}
-		>
-			{props.data.DDISH_NM.replace(/(\.|\s{2})|\([0-9가-힣/.]+\)/g, '').replace(/\<br\/?\>/g, '\n')}
-		</Embed>
-	),
-	NutritionEmbed: (props) => (
-		<Embed
-			title={props.title}
-			footer={`총 ${props.data.CAL_INFO.toLowerCase()}`}
-		>
-			{props.data.NTR_INFO.split(/\<br\/?\>/).map(ntr=> {
-				const [key, value] = ntr.split(" : ");
-				return (
-					<Field name={key} inline>
-						{value}
-					</Field>
-				);
-			})}
-		</Embed>
-	)
+	MenuEmbed,
+	NutritionEmbed
 };
