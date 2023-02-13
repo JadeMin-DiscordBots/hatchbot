902ce31713ccdad4ca6efeda29c413619139c6f5
JadeMin <alt0000@klartnet.ml>
2023-02-14 02:36:37 +0900

    update

--- src/messages/all.js
+++ src/messages/all.js
@@ -6,7 +6,7 @@ import { call } from 'slshx';
 const Logger = new WebLogger(env.LOGHOOK_ID, env.LOGHOOK_TOKEN);
 const botAuth = {bot: env.BOT_TOKEN};
 
-export default async (request) => {
+export default async (message) => {
 	if(message.content === "!tq중이야") {
 		await call('POST', Routes.channelMessages(message.channel_id), {
 			content: `<@${message.author.id}>, 아주 ㅅㅂ하다`
