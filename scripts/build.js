ccbfe5e79ca596aa1201aed7be7ac5629e759c62
JadeMin <alt0000@klartnet.ml>
2023-04-30 22:14:58 +0900

    update packages

--- scripts/build.js
+++ scripts/build.js
@@ -11,6 +11,7 @@ const define = {
 	"env.APPLICATION_ID": JSON.stringify(env["APPLICATION_ID"]),
 	"env.PUBLIC_KEY": JSON.stringify(env["PUBLIC_KEY"]),
 	"env.SECRET_KEY": JSON.stringify(env["SECRET_KEY"]),
+	"env.BOT_TOKEN": JSON.stringify(env["BOT_TOKEN"]),
 	
 	"env.LOGHOOK_ID": JSON.stringify(env["LOGHOOK_ID"]),
 	"env.LOGHOOK_TOKEN": JSON.stringify(env["LOGHOOK_TOKEN"]),
